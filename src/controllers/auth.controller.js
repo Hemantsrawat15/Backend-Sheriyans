a2ff0d19c2364b927630c4b28a2ccc373a564be6
Hemantsrawat15 <hemant.srawat15@gmail.com>
2026-03-31 18:14:52 +0530

    Login API Done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -53,6 +53,37 @@ async function userRegisterController(req, res) {
 
 }
 
+async function userLoginController(req,res){
+    const {email, password} = req.body;
+    const user = await userModel.findOne({email}).select("+password");
+
+    if(!user){
+        return res.status(401).json({
+            message: "Email or Password is Invalid",
+        })
+    }
+
+    const isValidPassword = user.comparePassword(password)
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message: "Email or password is Invalid",
+        })
+    }
+
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: "3d"});
+
+    res.status(200).json({
+        user:{
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
+}
+
 module.exports = {
-    userRegisterController
+    userRegisterController,
+    userLoginController
 }
--- src/routes/auth.model.js
+++ src/routes/auth.model.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/register",authController.userRegisterController);
+router.get("/login", authController.userLoginController);
 
 
 
